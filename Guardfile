21ad662c7a5d725905c97ede5dc1952ec4c98369
Jonathan Ng <ngng@google.com>
2015-11-05 17:05:07 -0800

    gem and Guard files

---
+++ Guardfile
@@ -0,0 +1,4 @@
+# https://github.com/guard/guard-livereload
+guard :livereload do
+  watch(%r{.+.(css|js|html)$})
+end
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,7 +13,7 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 
 # list of files/folders to symlink in homedir
-files="bash_aliases bashrc emacs gitconfig profile tmux.conf Xmodmap xinitrc"
+files="bash_aliases bashrc emacs gemrc gitconfig Guardfile profile tmux.conf Xmodmap xinitrc"
 
 ##########
 
